bfc4ddcd9980c4d87f3ce7c49dc669a872f07782
endrithashani <94977365+endrithashani@users.noreply.github.com>
2022-03-20 21:49:02 +0100

    Add files via upload

--- src/components/NestedToggle.js
+++ src/components/NestedToggle.js
@@ -1,5 +1,5 @@
 import { useState } from "react";
-function NestedToggle() {
+const NestedToggle=({closeModal}) =>{
   const[selected,setSelected]=useState(null);
   const Nestedtoggle = (i) =>{
     if(selected == i){
@@ -8,33 +8,32 @@ function NestedToggle() {
 
     setSelected(i)
   }
-  
-  return (
-    <div className="container-nested">
-    <div className="nested-container">
-    {datas.map((card , i)=>{
-      const{content,title}=card;
-      return(
-        
-        <div className="items">
-        <div className="contents" onClick={()=>Nestedtoggle(i)}>
-        <h4>{content}</h4>
-        <span>{selected === i ? '˄' : '˃'}</span>
-        </div>
-        <div className={selected === i ? 'titles show' : 'titles'}>
-        <p>{title}</p>
-        </div>
-        </div>
-        )
-    })}
-    </div>
-    </div>
-  );
-}
+    const[status,setStatus]=useState(true);
+    return (
+      <div className="container-nested">
+      <div className="nested-container">
+      {datas.map((card , i)=>{
+        const{content,title}=card;
+        return( 
+          <div className="items">
+          <div className="contents" onClick={()=>Nestedtoggle(i)}>
+          <h4>{content}</h4>
+          <span>{selected === i ? '˄' : '˃'}</span>
+          </div>
+          <div className={selected === i ? 'titles show' : 'titles'}>
+          <p>{title}</p>
+          </div>
+          </div>
+          )
+      })}
+      </div>
+      </div>
+    );
+  }
 const datas=[
   {
   content:"Why does Parkname seperate itself from other name parking companies ?",
-  title:"Your domain are a valuable online property. As in any investment, you want the most efficient,easy way to make sure your property is going to be profitable.Do you own more than 1000 domains? As a professional domainer, you will find everything you need through Parkname to generate maximum profits fromyour domain portfolio.",
+  title:"Your domain are a valuable online property. As in any investment, you want the most efficient,easy way to make sure your property is going to be profitable.Do you own more than 1000 domains? As a professional domainer, you will find everything you need through Parkname to generate maximum profits from your domain port",
   },
   {
   content:"Is Parkname Parking actually free?",
