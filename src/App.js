0f22782182e9e11057d0359706ba593f5d6f1a1d
endrithashani <94977365+endrithashani@users.noreply.github.com>
2022-03-20 21:49:20 +0100

    Add files via upload

--- src/App.js
+++ src/App.js
@@ -1,13 +1,25 @@
 import Tabs from './components/Tabs';
 import Toggle from './components/Toggle';
 import NestedToggle from './components/NestedToggle';
-
+import {useState} from 'react';
+const image= require('./components/images/user.png');
 function App() {
+  const [openToggle,setOpenToggle] = useState(false);
   return (
     <div className="App"> 
     <Tabs/>
     <Toggle/>
-    <NestedToggle/>
+    <button onClick={()=>{setOpenToggle(true);}} className='btn-toggle'>
+    <img src={image} alt='about'></img>
+    <div className="inside-button">
+    <h3>About Us</h3>
+    <p>4 articles in this Topic</p>
+    </div>
+    <div className="arrow">
+    <p>⮟</p>
+    </div>
+    </button>
+    {openToggle && <NestedToggle closeToggle={setOpenToggle}/>}
     </div>
   );
 }
