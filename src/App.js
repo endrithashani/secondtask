bc2d44f5bdcfb3d8ffa4a15b4d723f4b80fae839
endrithashani <94977365+endrithashani@users.noreply.github.com>
2022-03-24 21:35:25 +0100

    Add files via upload

--- src/App.js
+++ src/App.js
@@ -1,25 +1,12 @@
-import Tabs from './components/Tabs';
-import Toggle from './components/Toggle';
-import NestedToggle from './components/NestedToggle';
-import {useState} from 'react';
-const image= require('./components/images/user.png');
+import Modal from './components/Modal'
+import {useState} from 'react'
+
 function App() {
-  const [openToggle,setOpenToggle] = useState(false);
+  const [openModal,setOpenModal] = useState(false);
   return (
-    <div className="App"> 
-    <Tabs/>
-    <Toggle/>
-    <button onClick={()=>{setOpenToggle(true);}} className='btn-toggle'>
-    <img src={image} alt='about'></img>
-    <div className="inside-button">
-    <h3>About Us</h3>
-    <p>4 articles in this Topic</p>
-    </div>
-    <div className="arrow">
-    <p>⮟</p>
-    </div>
-    </button>
-    {openToggle && <NestedToggle closeToggle={setOpenToggle}/>}
+    <div className="App">
+    <button onClick={()=>{setOpenModal(true);}} className='btn-modal'>MODAL</button>
+    {openModal && <Modal closeModal={setOpenModal}/>}
     </div>
   );
 }
